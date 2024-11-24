1ecff9443d72ccc18f0503df0dccb3bf97d021dc
anna_vezhdel <avezhdel@gmail.com>
2024-11-24 10:56:30 +0100

    fix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,11 @@
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 import s from "./Navigation.module.css";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   const buildLinkClass = ({ isActive }) => {
     return clsx(s.link, isActive && s.activeLink);
   };
@@ -11,9 +14,11 @@ const Navigation = () => {
       <NavLink to="/" className={buildLinkClass}>
         Home
       </NavLink>
-      <NavLink to="/contacts" className={buildLinkClass}>
-        Contacts
-      </NavLink>
+      {isLoggedIn && (
+        <NavLink className={buildLinkClass} to="/contacts">
+          Contacts
+        </NavLink>
+      )}
     </div>
   );
 };
--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,13 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
 import ContactForm from "../components/ContactForm/ContactForm";
 import ContactList from "../components/ContactList/ContactList";
 import SearchBox from "../components/SearchBox/SearchBox";
+import { useEffect } from "react";
+import { fetchContacts } from "../redux/contacts/operations";
+import { selectIsError, selectIsLoading } from "../redux/contacts/selectors";
 
 const ContactsPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  const isLoading = useSelector(selectIsLoading);
+  const isError = useSelector(selectIsError);
+
   return (
     <div>
       <h1 className="text-center font-bold">Phonebook</h1>
       <ContactForm />
       <SearchBox />
+      {isLoading && <h2>Loading...</h2>}
+      {isError && <h2>Error...</h2>}
       <ContactList />
     </div>
   );
